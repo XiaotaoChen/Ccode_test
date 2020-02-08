4a46235be696b78402e38fd021a787235cf2539e
xiaotao.chen <xiaotao.chen@tusimple.ai>
2020-02-08 12:00:43 +0800

    add fix syncthreads bug

--- test_cuda/scan.cu
+++ test_cuda/scan.cu
@@ -2,8 +2,9 @@
 #include<vector>
 #include<string>
 #include<cuda_runtime.h>
-#include <thrust/sequence.h>
+// #include <thrust/sequence.h>
 #include <thrust/scan.h>
+#include <thrust/execution_policy.h> // thrust::host/device
 
 #define BLOCK_SIZE 32
 
@@ -20,23 +21,23 @@ float average(const vector<float> &timing) {
   return avg;
 }
 
-void print_info(float *data, int len, string flag) {
+void print_info(int *data, int len, string flag) {
     printf("%s frist ten:\n", flag.c_str());
     for (int i=0; i<10; i++){
-        printf("%.4lf ", data[i]);
+        printf("%d ", data[i]);
     }
     printf("\n");
     printf("%s last ten:\n", flag.c_str());
     for (int i=len -10; i<len; i++){
-        printf("%.4lf ", data[i]);
+        printf("%d ", data[i]);
     }
     printf("\n");
 }
 
 __global__ void exclusive_scan(int *A, int N) {
-    int thx = threadIdx.x;
+    int thx = blockDim.x * blockIdx.x + threadIdx.x;
     if (thx >= N) return;
-    int logN = 0;
+    __shared__ int logN;
     if (thx == 0) {
         logN = log2f(N);
     }
@@ -58,7 +59,8 @@ __global__ void exclusive_scan(int *A, int N) {
         int dd2 = d2/2;
         if (thx%d2==d2-1) {
             int tmp = A[thx-dd2];
-            A[thx-dd2] = tmp;
+            // A[thx-dd2] = tmp;
+            A[thx-dd2] = A[thx];
             A[thx] += tmp;
         }
         __syncthreads();
@@ -70,28 +72,29 @@ int main() {
     int numThreads = BLOCK_SIZE;
     int numBlocks = (len_a + numThreads - 1) / numThreads;
 
-    float* A = (float*) malloc(len_a * sizeof(float));
-    float* th_h_A = (float*) malloc(len_a * sizeof(float));
+    int* A = (int*) malloc(len_a * sizeof(int));
+    int* th_h_A = (int*) malloc(len_a * sizeof(int));
     // file a,b
     for (int i=0; i<len_a; i++) A[i] = i;
     for (int i=0; i<len_a; i++) th_h_A[i] = i;
-    print_info(A, "A");
+    print_info(A, len_a, "A");
 
-    float *d_A, *th_A;
-    cudaMalloc((void**)&d_A, len_a * sizeof(float));
-    cudaMalloc((void**)&th_A, len_a * sizeof(float));
+    int *d_A, *th_A;
+    cudaMalloc((void**)&d_A, len_a * sizeof(int));
+    cudaMalloc((void**)&th_A, len_a * sizeof(int));
 
-    cudaMemcpy(d_A, A, len_a * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(th_A, A, len_a * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_A, A, len_a * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(th_A, A, len_a * sizeof(int), cudaMemcpyHostToDevice);
 
     // thrust scan
-    thrust::exclusive_scan(th_A, th_A+len_a, th_A);
-    cudaMemcpy(th_h_A, th_A, len_a * sizeof(float), cudaMemcpyDeviceToHost);
+    thrust::exclusive_scan(thrust::device, th_A, th_A+len_a, th_A, 0);
+    cudaMemcpy(th_h_A, th_A, len_a * sizeof(int), cudaMemcpyDeviceToHost);
+    // thrust::exclusive_scan(thrust::host, th_h_A, th_h_A+len_a, th_h_A, 0);
 
     print_info(th_h_A, len_a, "th_h_A");
 
     vector<float> times;
-    int loops = 3;
+    int loops = 1;
     for (int i=0; i<loops; i++) {
         cudaEvent_t start, end;
         cudaEventCreate(&start);
