b2e9516b99e1b167d7e4178ec04b7cde0d6bccbc
xiaotao.chen <xiaotao.chen@tusimple.ai>
2020-02-09 18:47:19 +0800

    [WIP] support reuce

--- test_cuda/reduce.cu
+++ test_cuda/reduce.cu
@@ -50,13 +50,14 @@ void print_device_info(int *device_data, int len, string flag) {
 }
 
 // interleved addressing, divergent warps branch
+// time: 33.3 ms
 __global__ void reduce_kernel1(int*A, int*out, int len_a) {
     int idx = blockDim.x * blockIdx.x + threadIdx.x;
-    if (idx>=len_a) return;
     int bid = blockIdx.x;
     int tid = threadIdx.x;
     extern __shared__ int sA[];
-    sA[tid] = A[idx];
+    sA[tid] = 0;
+    if (idx < len_a) sA[tid] = A[idx];
     __syncthreads();
     for (int s=1; s<blockDim.x; s*=2) {
         if(tid%(2*s)== 0) {
@@ -68,13 +69,14 @@ __global__ void reduce_kernel1(int*A, int*out, int len_a) {
 }
 
 // inerleved addressing, bands conflict
+// time: 20 ms
 __global__ void reduce_kernel2(int*A, int*out, int len_a) {
     int idx = blockDim.x * blockIdx.x + threadIdx.x;
-    if (idx>=len_a) return;
     int bid = blockIdx.x;
     int tid = threadIdx.x;
     extern __shared__ int sA[];
-    sA[tid] = A[idx];
+    sA[tid] = 0;
+    if (idx < len_a) sA[tid] = A[idx];
     __syncthreads();
     for (int s=1; s<blockDim.x; s*=2) {
         int run_id = 2 * s * tid;
@@ -87,17 +89,18 @@ __global__ void reduce_kernel2(int*A, int*out, int len_a) {
 }
 
 // sequential addressing
+// time: 17.9 ms
 __global__ void reduce_kernel3(int*A, int*out, int len_a) {
     int idx = blockDim.x * blockIdx.x + threadIdx.x;
-    if (idx>=len_a) return;
     int bid = blockIdx.x;
     int tid = threadIdx.x;
     extern __shared__ int sA[];
-    sA[tid] = A[idx];
+    sA[tid] = 0;
+    if (idx < len_a) sA[tid] = A[idx];
     __syncthreads();
     for (int s=blockDim.x/2; s>0; s>>=1) {
         if(tid <s) {
-            if (tid+s < blockDim.x) sA[tid] += sA[tid+s];
+          sA[tid] += sA[tid+s];
         }
         __syncthreads();
     }
@@ -105,51 +108,183 @@ __global__ void reduce_kernel3(int*A, int*out, int len_a) {
 }
 
 // first add during load
+// time:10.78 ms
 __global__ void reduce_kernel4(int*A, int*out, int len_a) {
     int idx = blockDim.x * (blockIdx.x * 2) + threadIdx.x;
     int bid = blockIdx.x;
     int tid = threadIdx.x;
     extern __shared__ int sA[];
-    if (idx + blockDim.x < len_a) {
-        sA[tid] = A[idx] + A[blockDim.x + idx];
-    }
-    else{
-        sA[tid] = A[idx];
-    }
+    sA[tid] = 0;
+    if (idx < len_a) sA[tid] = A[idx];
+    if (idx + blockDim.x < len_a) sA[tid] += A[idx + blockDim.x];
     
     __syncthreads();
     for (int s=blockDim.x/2; s>0; s>>=1) {
         if(tid <s) {
-            if (tid+s < blockDim.x) sA[tid] += sA[tid+s];
+            sA[tid] += sA[tid+s];
         }
         __syncthreads();
     }
     if (tid==0) out[bid] = sA[0];
 }
 
-__global__ void copy(int*A, int*out, int len_a) {
-    int idx = blockDim.x * blockIdx.x + threadIdx.x;
-    if (idx >= len_a) return;
-    out[idx] = A[idx];
+// unroll the last warp
+__device__ void warpFunc(volatile int* sdata, int tid) {
+    sdata[tid] += sdata[tid+32];
+    sdata[tid] += sdata[tid+16];
+    sdata[tid] += sdata[tid+8];
+    sdata[tid] += sdata[tid+4];
+    sdata[tid] += sdata[tid+2];
+    sdata[tid] += sdata[tid+1];
+}
+// time: 8.0 ms
+__global__ void reduce_kernel5(int *A, int *out, int len_a) {
+    int idx = blockDim.x * (blockIdx.x * 2) + threadIdx.x;
+    int bid = blockIdx.x;
+    int tid = threadIdx.x;
+    extern __shared__ int sA[];
+    sA[tid] = 0;
+    if (idx < len_a) sA[tid] = A[idx];
+    if (idx + blockDim.x < len_a) sA[tid] += A[idx + blockDim.x];
+    
+    __syncthreads();
+    for (int s=blockDim.x/2; s>32; s>>=1) {
+        if(tid <s) {
+            sA[tid] += sA[tid+s];
+        }
+        __syncthreads();
+    }
+    if (tid<32) warpFunc(sA, tid);
+    if (tid==0) out[bid] = sA[0];
 }
 
+// completely unroll
+// time: 8.03 ms
+__global__ void reduce_kernel6(int *A, int *out, int len_a) {
+    int idx = blockDim.x * (blockIdx.x * 2) + threadIdx.x;
+    int bid = blockIdx.x;
+    int tid = threadIdx.x;
+    extern __shared__ int sA[];
+    sA[tid] = 0;
+    if (idx < len_a) sA[tid] = A[idx];
+    if (idx + blockDim.x < len_a) sA[tid] += A[idx + blockDim.x];
+    __syncthreads();
+
+    if (blockDim.x >= 1024) { if (tid < 512) sA[tid] += sA[tid+512]; __syncthreads();}
+    if (blockDim.x >= 512) { if (tid < 256) sA[tid] += sA[tid+256]; __syncthreads();}
+    if (blockDim.x >= 256) { if (tid < 128) sA[tid] += sA[tid+128]; __syncthreads();}
+    if (blockDim.x >= 128) { if (tid < 64) sA[tid] += sA[tid+64]; __syncthreads();}
+    if (tid<32) warpFunc(sA, tid);
+    // if (tid<32) {
+    //     sA[tid]+= sA[tid+32];
+    //     sA[tid]+= sA[tid+16];
+    //     sA[tid]+= sA[tid+8];
+    //     sA[tid]+= sA[tid+4];
+    //     sA[tid]+= sA[tid+2];
+    //     sA[tid]+= sA[tid+1];
+    // }
+    if (tid==0) out[bid] = sA[0];
+}
+
+template <unsigned int blockSize>
+__device__ void warpReduce(volatile int *sdata, int tid) {
+    if (blockSize >=64) sdata[tid] += sdata[tid+32];
+    if (blockSize >=32) sdata[tid] += sdata[tid+16];
+    if (blockSize >=16) sdata[tid] += sdata[tid+8];
+    if (blockSize >=8) sdata[tid] += sdata[tid+4];
+    if (blockSize >=4) sdata[tid] += sdata[tid+2];
+    if (blockSize >=2) sdata[tid] += sdata[tid+1];
+}
+
+// load as much as data
+__global__ void reduce_kernel7(int *A, int *out, int len_a) {
+    int idx = blockDim.x * blockIdx.x + threadIdx.x;
+    int bid = blockIdx.x;
+    int tid = threadIdx.x;
+    int griz = gridDim.x * blockDim.x;
+    extern __shared__ int sA[];
+    sA[tid] = 0;
+    int tmp_id = idx;
+    while(tmp_id <len_a) {
+        if (tmp_id < len_a) sA[tid] = A[tmp_id];
+        tmp_id += griz;
+    }
+    __syncthreads();
+    if (blockDim.x >= 1024) { if (tid < 512) sA[tid] += sA[tid+512]; __syncthreads();}
+    if (blockDim.x >= 512) { if (tid < 256) sA[tid] += sA[tid+256]; __syncthreads();}
+    if (blockDim.x >= 256) { if (tid < 128) sA[tid] += sA[tid+128]; __syncthreads();}
+    if (blockDim.x >= 128) { if (tid < 64) sA[tid] += sA[tid+64]; __syncthreads();}
+    if (tid<32) warpFunc(sA, tid);
+    if (tid==0) out[bid] = sA[0];
+}
+
+
 int reduce(int*A, int len_a, int numThreads){
-    // numBlocks = (len_a + numThreads) / numThreads;
-    int numBlocks = (len_a + numThreads*2 -1) / numThreads/2;
+    int numBlocks = (len_a + numThreads) / numThreads;
+    // int numBlocks = (len_a + numThreads*2 -1) / numThreads/2;
     int*tmp;
     cudaMalloc((void**)&tmp, numBlocks*sizeof(int));
     while(len_a>1) {
         printf("len:%d, numBlocks:%d, numThreads:%d\n", len_a, numBlocks, numThreads);
         // reduce_kernel1<<<numBlocks, numThreads, numThreads*sizeof(int)>>>(A, tmp, len_a);
-        // reduce_kernel2<<<numBlocks, numThreads, numThreads*sizeof(int)>>>(A, tmp, len_a);
-        // reduce_kernel3<<<numBlocks, numThreads, numThreads*sizeof(int)>>>(A, tmp, len_a);
-        // len_a = numBlocks;
-        // numBlocks = (numBlocks+numThreads-1)/numThreads;
+        // // time: 33.3 ms
 
-        reduce_kernel4<<<numBlocks, numThreads, numThreads*sizeof(int)>>>(A, tmp, len_a);
+        // reduce_kernel2<<<numBlocks, numThreads, numThreads*sizeof(int)>>>(A, tmp, len_a);
+        // // time: 20 ms
+
+        reduce_kernel3<<<numBlocks, numThreads, numThreads*sizeof(int)>>>(A, tmp, len_a);
+        // time: 17.9 ms
+        
         len_a = numBlocks;
-        numBlocks = (len_a + numThreads*2 -1) / numThreads/2;
-        copy<<<numBlocks, numThreads>>>(tmp, A, len_a);
+        numBlocks = (numBlocks+numThreads-1)/numThreads;
+        cudaMemcpy(A, tmp, sizeof(int) * len_a, cudaMemcpyDeviceToDevice);
+        // print_device_info(tmp, len_a, "tmp");
+    }
+    int result;
+    cudaMemcpy(&result, tmp, sizeof(int), cudaMemcpyDeviceToHost);
+    return result;
+}
+
+int reduce_2(int*A, int len_a, int numThreads){
+    int numBlocks = (len_a + numThreads*2 -1) / numThreads/2;
+    int*tmp;
+    cudaMalloc((void**)&tmp, numBlocks*sizeof(int));
+    while(len_a>1) {
+        printf("len:%d, numBlocks:%d, numThreads:%d\n", len_a, numBlocks, numThreads);
+        
+        // reduce_kernel4<<<numBlocks, numThreads, numThreads*sizeof(int)>>>(A, tmp, len_a);
+        // // time:10.78 ms
+
+        // reduce_kernel5<<<numBlocks, numThreads, numThreads*sizeof(int)>>>(A, tmp, len_a);
+        // // time: 8.0 ms
+
+        reduce_kernel6<<<numBlocks, numThreads, numThreads*sizeof(int)>>>(A, tmp, len_a);
+        // time: 8.03 ms
+
+        len_a = numBlocks;
+        numBlocks = (numBlocks+numThreads*2-1)/numThreads/2;
+        cudaMemcpy(A, tmp, sizeof(int) * len_a, cudaMemcpyDeviceToDevice);
+        // print_device_info(tmp, len_a, "tmp");
+    }
+    int result;
+    cudaMemcpy(&result, tmp, sizeof(int), cudaMemcpyDeviceToHost);
+    return result;
+}
+
+int reduce_3(int*A, int len_a, int numThreads){
+    // int count_per_threads = 2;
+    // int count_per_blocks = 2;
+    int numBlocks = (len_a + numThreads*8 -1) / numThreads/8;
+    int*tmp;
+    cudaMalloc((void**)&tmp, numBlocks*sizeof(int));
+    while(len_a>1) {
+        printf("len:%d, numBlocks:%d, numThreads:%d\n", len_a, numBlocks, numThreads);
+        reduce_kernel7<<<numBlocks, numThreads, numThreads*sizeof(int)>>>(A, tmp, len_a);
+        // time:  ms
+
+        len_a = numBlocks;
+        numBlocks = (numBlocks+numThreads*8-1)/numThreads/8;
+        cudaMemcpy(A, tmp, sizeof(int) * len_a, cudaMemcpyDeviceToDevice);
         // print_device_info(tmp, len_a, "tmp");
     }
     int result;
@@ -162,7 +297,6 @@ int main() {
     // int len_a = 256 * 256 * 256;
     int len_a = 1024*1024*1024;
     int numThreads = BLOCK_SIZE;
-    int numBlocks = (len_a + numThreads - 1) / numThreads;
 
     int* A = (int*) malloc(len_a * sizeof(int));
     int* th_h_A = (int*) malloc(len_a * sizeof(int));
@@ -192,7 +326,8 @@ int main() {
         cudaEventCreate(&end);
         cudaEventRecord(start, NULL);
 
-        int result = reduce(d_A, len_a, numThreads);
+        // int result = reduce(d_A, len_a, numThreads);
+        int result = reduce_2(d_A, len_a, numThreads);
         printf("result:%d\n", result);
 
         cudaEventRecord(end, NULL);
@@ -202,5 +337,10 @@ int main() {
         times.push_back(time);
     }
     printf("reduce avg time:%lf\n", average(times));
+
+    free(A);
+    free(th_h_A);
+    cudaFree(d_A);
+    cudaFree(th_A);
 
 }
