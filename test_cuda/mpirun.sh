a2a2e2623affeba8eae5f676ad413332e03e5743
xiaotao.chen <xiaotao.chen@tusimple.ai>
2020-05-04 23:35:23 +0800

    add nccl sync, but dont work when device is 2

---
+++ test_cuda/mpirun.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+workspace=/mnt/truenas/scratch/xiaotao.chen/Repositories/Ccode_test/test_cuda
+
+num_proc=1
+hostfile=local.txt
+single_script=./test
+
+
+/root/3rdparty/openmpi4.0/bin/mpirun -np ${num_proc} \
+    --allow-run-as-root \
+    --npernode 1 \
+    --hostfile ${hostfile} \
+    -bind-to none -map-by slot \
+    -x NCCL_DEBUG=INFO \
+    -x LD_LIBRARY_PATH \
+    -x PATH \
+    -x PYTHONPATH \
+    -x OMP_NUM_THREADS=1 \
+    -mca pml ob1 \
+    -mca btl ^openib \
+    ${single_script}
+
